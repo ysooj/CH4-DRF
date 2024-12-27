87dd073a106603e3e89c1cc561d2edcccc0a44ef
ysooj <musukie52@gmail.com>
2024-12-27 09:38:03 +0900

    feat: settings.py 원상복구

--- spartamarket_DRF/settings.py
+++ spartamarket_DRF/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.DJANGO_SECRET_KEY
+SECRET_KEY = 'django-insecure-t0x3rg^^uf(eeg_oge5%@rj2f!l6i3p1j@3h-pnv)uy4&zmkei'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
